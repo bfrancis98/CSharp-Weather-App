81f09573734219c754d8c3c72bc7f4def0d845ce
bfrancis98 <brandonfrancis98@gmail.com>
2024-03-14 12:04:22 -0400

    settings set up
    changed ParseLatLon into general parser.
    created settings setup
    no save to file yet

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Transactions;
+using System.IO;
+using System.Text;
 
 namespace CSharp_Weather_App;
 
 class Program
 {
     static string apiKey = "";
+    static string settingsPath = "";
     static readonly HttpClient client = new HttpClient();
     static string lat = "";
     static string lon = "";
@@ -15,26 +18,25 @@ class Program
 
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Enter your API key: ");
-        apiKey = Console.ReadLine();
-        Console.WriteLine("Enter your latitude and longitude (lat,lon): ");
-        ParseLatLon(Console.ReadLine());
-        Console.WriteLine("Imperial or metric?: ");
-        UnitHandler(Console.ReadLine());
+        SettingsSetUp();
+        /*
+        
+        */
+        
     }
 
-    static void ParseLatLon(string value)
+    // not a fan of using string separator, but new line character is a string
+    // wonder if i should validate separator...
+    static string[] Parser(string value, string separator)
     {
-        char separator = ',';
-        string[] coords = value.Split(separator);
-        lat = coords[0];
-        lon = coords[1];
+        string[] result = value.Split(separator);
+        return result;
     }
 
     static void UnitHandler(string value)
     {
         string workingString = value.ToLower();
-        
+    
         switch(workingString)
         {
             case "imperial":
@@ -45,4 +47,49 @@ class Program
                 break;
         }
     }
+
+    static void SettingsSetUp()
+    {
+        if (File.Exists("settings.txt"))
+        {
+            // prob want to check if there is valid data in the settings
+            // load variables here
+            string settings = File.ReadAllText("settings.txt");
+            string[] workingSettings = Parser(settings, Environment.NewLine);
+            apiKey = workingSettings[0];
+            lat = workingSettings[1];
+            lon = workingSettings[2];
+            units = workingSettings[3];
+
+        }  
+        else 
+        {
+            File.Create("settings.txt");
+            UserSetUp();
+        }
+    }
+
+
+    static void UserSetUp()
+    {
+        // need error handling
+
+        Console.WriteLine("Enter your API key: ");
+        apiKey = Console.ReadLine();
+        Console.WriteLine("Enter your latitude and longitude (lat,lon): ");
+        Parser(Console.ReadLine(), ",");
+        Console.WriteLine("Imperial or metric?: ");
+        UnitHandler(Console.ReadLine());
+    }
 }
+/*
+Settings looks like this:
+L1: apikey
+L2: lat
+L3: lon
+L4: units
+
+should separate with new line characters
+to make it more complicated, i could label the different lines
+and remove the labels from the lines to get the values...
+*/
--- obj/Debug/net8.0/CSharp-Weather-App.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharp-Weather-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp-Weather-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00dbfef074bba370813c2d64d9d4c08fc4429583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00096c2b54f6199e0859c4c5ed4415c76a4c9b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp-Weather-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp-Weather-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
